ae29b60b28e387ce8ea205d4871d8df446e93dcd
Catriona Ford <catriona.b.ford@gmail.com>
2019-01-04 17:29:31 +0000

    all unit tests passing

--- catriona_ford_PDA/Task B Unit and Integration Testing JS/js_calculator_start_point/tests/unit/calculator_spec.js
+++ catriona_ford_PDA/Task B Unit and Integration Testing JS/js_calculator_start_point/tests/unit/calculator_spec.js
@@ -11,4 +11,43 @@ describe('calculator', function () {
     assert.equal(true, true)
   })
 
+  // unit test for starting at zero
+  it('should start with zero for the previous total', function () {
+    const actual = calculator.previousTotal;
+    assert.strictEqual(actual, 0);
+  });
+
+  it('should start with zero for the running total', function () {
+    const actual = calculator.runningTotal;
+    assert.strictEqual(actual, 0);
+  });
+
+  it('should be able to add to the previous total', function () {
+    calculator.previousTotal = 4;
+    calculator.add(1);
+    const actual = calculator.runningTotal;
+    assert.strictEqual(actual, 5);
+  });
+
+  it('should be able to subtract from the previous total', function () {
+    calculator.previousTotal = 7;
+    calculator.subtract(4);
+    const actual = calculator.runningTotal;
+    assert.strictEqual(actual, 3);
+  });
+
+  it('should be able to multiply the previous total', function () {
+    calculator.previousTotal = 3;
+    calculator.multiply(5);
+    const actual = calculator.runningTotal;
+    assert.strictEqual(actual, 15);
+  });
+
+  it('should be able to divide the previous total', function () {
+    calculator.previousTotal = 21;
+    calculator.divide(7);
+    const actual = calculator.runningTotal;
+    assert.strictEqual(actual, 3);
+  });
+
 });
