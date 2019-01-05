838086a45d7afce82a6a2d06e2a3dd8ff4987199
Catriona Ford <catriona.b.ford@gmail.com>
2019-01-05 20:42:23 +0000

    chai tests for working number and operator buttons passing

--- catriona_ford_PDA/Task B Unit and Integration Testing JS/js_calculator_start_point/tests/integration/tests.js
+++ catriona_ford_PDA/Task B Unit and Integration Testing JS/js_calculator_start_point/tests/integration/tests.js
@@ -16,4 +16,14 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('2')
   })
 
+  it('should have working operator buttons', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#operator_add')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#operator_add')).click();
+    element(by.css('#number6')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('8')
+  })
+
 });
